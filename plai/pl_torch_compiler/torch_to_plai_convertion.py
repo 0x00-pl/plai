76b4e9a00c7a2ef5590a572cb1c0dc357ab98ec0
0x00-pl <0x00.pl@gmail.com>
2024-09-07 18:51:17 +0800

    rewrite torch_to_plai_convertion.py

--- plai/pl_torch_compiler/torch_to_plai_convertion.py
+++ plai/pl_torch_compiler/torch_to_plai_convertion.py
@@ -1,15 +1,13 @@
 import importlib
-import inspect
-from typing import Callable, Any
+from typing import Callable, Any, Dict
 
 import torch
 from torch import fx
 from torch._ops import OpOverload
 
 from plai.core import core_dialect
-from plai.core.location import NamedLocation
+from plai.core.location import NamedLocation, Location
 from plai.core.module import Node
-from plai.dialect import aten_dialect, torch_dialect
 
 
 def get_object_from_string(full_path):
@@ -26,61 +24,49 @@ def torch_module_to_string(module: torch.nn.Module) -> str:
     return module.__class__.__name__
 
 
-def torch_function_to_string(func: Callable) -> str:
+def torch_function_to_string(func: Callable):
     if isinstance(func, OpOverload):
         return func.name()
     else:
-        full_name = f'{inspect.getmodule(func).__name__}.{func.__name__}'
-        assert get_object_from_string(full_name) == func
-        return full_name
+        return func
+        # full_name = f'{inspect.getmodule(func).__name__}.{func.__name__}'
+        # assert get_object_from_string(full_name) == func
+        # return full_name
 
 
-def convert_node(node: fx.Node, node_mapping: Callable[[fx.Node], Any]) -> Node:
-    if node.op == 'placeholder':
-        assert isinstance(node.target, str)
-        return core_dialect.Placeholder(NamedLocation(node.target))
-    elif node.op == 'call_method':
-        raise NotImplementedError("call_method is not supported")
-    elif node.op == 'call_module':
-        raise NotImplementedError("call_module is not supported")
-    elif node.op == 'call_function':
-        func_name = torch_function_to_string(node.target)
-        args = [node_mapping(arg) for arg in node.args]
-        attrs = {k: node_mapping(v) for k, v in node.kwargs.items()}
-        if func_name == 'aten::view':
-            return aten_dialect.View(args[0], args[1], NamedLocation(node.name))
-        elif func_name == 'aten::detach':
-            return aten_dialect.Relu(args[0], NamedLocation(node.name))
-        elif func_name == 'aten::t':
-            return core_dialect.Transpose(args[0], NamedLocation(node.name))
-        elif func_name == 'aten::addmm':
-            return aten_dialect.AddMm(
-                args[0], args[1], args[2],
-                attrs.get('beta', 1), attrs.get('alpha', 1),
-                NamedLocation(node.name)
-            )
-        elif func_name == 'aten::mm':
-            return aten_dialect.Mm(args[0], args[1], NamedLocation(node.name))
-        elif func_name == 'aten::relu':
-            return aten_dialect.Relu(args[0], NamedLocation(node.name))
-        elif func_name == 'aten::max.dim':
-            keepdim = args[2] if len(args) == 3 else False
-            return aten_dialect.Max(args[0], args[1], keepdim, NamedLocation(node.name))
-        elif func_name == 'aten::sum.dim_IntList':
-            return aten_dialect.Sum(args[0], args[1], args[2], NamedLocation(node.name))
-        elif func_name == 'aten::threshold_backward':
-            return aten_dialect.ThresholdBackward(args[0], args[1], args[2], NamedLocation(node.name))
-        elif func_name == 'torch._C._nn.linear':
-            return torch_dialect.Linear(args[0], args[1], args[2], NamedLocation(node.name))
-        elif func_name == 'torch.relu':
-            return torch_dialect.Relu(args[0], NamedLocation(node.name))
-        elif func_name == '_operator.getitem':
-            return torch_dialect.GetItem(args[0], args[1], NamedLocation(node.name))
+class Converter:
+    def __init__(self):
+        self.node_converter_dict = {}
+
+    def register_convertion_function_dict(self, convertion_function_dict: Dict[str, Callable[[fx.Node], Node]]) -> None:
+        for k, v in convertion_function_dict.items():
+            assert k not in self.node_converter_dict, f"Duplicate key: {k}"
+        self.node_converter_dict.update(convertion_function_dict)
+
+    def get_converter(self, target) -> Callable[[list, dict, Location], Node]:
+        if isinstance(target, OpOverload):
+            func = target.name()
         else:
-            raise NotImplementedError(f"Unsupported function: {func_name}")
-    elif node.op == 'get_attr':
-        raise NotImplementedError("get_attr is not supported")
-    elif node.op == 'output':
-        raise ValueError("Do not put output node in the middle of the graph")
-    else:
-        raise ValueError(f"Unsupported op: {node.op}")
+            func = target
+        assert func in self.node_converter_dict, f"Unregistered function: {func}"
+        return self.node_converter_dict.get(func)
+
+    def convert_node(self, node: fx.Node, node_mapping: Callable[[fx.Node], Any]) -> Node:
+        if node.op == 'placeholder':
+            assert isinstance(node.target, str)
+            return core_dialect.Placeholder(NamedLocation(node.target))
+        elif node.op == 'call_method':
+            raise NotImplementedError("call_method is not supported")
+        elif node.op == 'call_module':
+            raise NotImplementedError("call_module is not supported")
+        elif node.op == 'call_function':
+            args = [node_mapping(arg) for arg in node.args]
+            attrs = {k: node_mapping(v) for k, v in node.kwargs.items()}
+            converter = self.get_converter(node.target)
+            return converter(args, attrs, NamedLocation(node.name))
+        elif node.op == 'get_attr':
+            raise NotImplementedError("get_attr is not supported")
+        elif node.op == 'output':
+            raise ValueError("Do not put output node in the middle of the graph")
+        else:
+            raise ValueError(f"Unsupported op: {node.op}")
