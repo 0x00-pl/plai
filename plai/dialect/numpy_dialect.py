0873bc7162fc48c0dc07e9200160bad873a839ba
0x00-pl <0x00.pl@gmail.com>
2024-09-21 01:50:32 +0800

    remove build method in numpy_dialect.py

--- plai/dialect/numpy_dialect.py
+++ plai/dialect/numpy_dialect.py
@@ -3,9 +3,6 @@ from plai.core.location import Location
 
 
 class NumpyNode(module.Node):
-    @staticmethod
-    def build(op_name: str, args: list, attrs: dict, loc: Location = None):
-        raise ValueError('this is a dialect, should not using Build.')
 
     @classmethod
     def get_namespace(cls):
@@ -15,8 +12,3 @@ class NumpyNode(module.Node):
 class Relu(NumpyNode):
     def __init__(self, arg: module.Node, loc: Location = None):
         super().__init__([arg], {}, loc)
-
-    @staticmethod
-    def build(op_name: str, args: list, attrs: dict, loc: Location = None):
-        assert op_name == 'relu'
-        return Relu(args[0], loc)
