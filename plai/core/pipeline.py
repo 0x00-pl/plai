b02e29b6d7260eceeecea1bf392e8468e70748f8
0x00-pl <0x00.pl@gmail.com>
2024-10-07 10:20:46 +0800

    add pipeline

--- plai/core/pipeline.py
+++ plai/core/pipeline.py
@@ -2,24 +2,66 @@ import typing
 
 
 class Pass:
-    def __init__(self, name, fn):
+    def __init__(self, name):
         self.name = name
-        self.fn = fn
 
-    def __call__(self, x):
-        return self.fn(x)
+    def __call__(self, graph) -> bool:
+        """
+        :param graph:
+        :return: True when changed.
+        """
+        return False
 
     def __repr__(self):
         return f"Pass({self.name})"
 
 
+class FnPass(Pass):
+    def __init__(self, fn):
+        super().__init__(fn.__name__)
+        self.fn = fn
+
+    def __call__(self, graph) -> bool:
+        """
+        :param graph:
+        :return: True when changed.
+        """
+        return self.fn(graph)
+
+
+class UntilStablePass(Pass):
+    def __init__(self, name: str = None, step: Pass = None):
+        super().__init__(name or f'until_stable')
+        self.step = step
+
+    def __call__(self, graph) -> bool:
+        changed = True
+        while changed:
+            changed = self.step(graph)
+        return changed
+
+    def __repr__(self):
+        return f"UntilStable({repr(self.step)})"
+
+
 class Pipeline(Pass):
     def __init__(self, name: str = None, steps: typing.List[Pass] = None, metadata: dict = None):
-        super().__init__(name or f'pipeline', self.call_steps)
+        super().__init__(name or f'pipeline')
         self.steps = steps or []
         self.metadata = metadata or {}
 
-    def call_steps(self, x):
+    def __call__(self, graph) -> bool:
+        changed = False
         for step in self.steps:
-            x = step(x)
-        return x
+            step_changed = step(graph)
+            changed = changed or step_changed
+        return changed
+
+    def __repr__(self):
+        return f"Pipeline({repr(self.steps)})"
+
+    def add_step(self, step: Pass):
+        self.steps.append(step)
+
+
+
