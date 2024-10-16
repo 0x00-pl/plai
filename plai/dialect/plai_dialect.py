18c96d0b5800f48c019e85f2f7f142fc1437ed95
0x00-pl <0x00.pl@gmail.com>
2024-10-16 22:45:13 +0800

    add more op on plai dialect

--- plai/dialect/plai_dialect.py
+++ plai/dialect/plai_dialect.py
@@ -14,3 +14,24 @@ class Transpose(PlaiNode):
     def __init__(self, arg: module.Node, permutation: List = None, loc: Location = None):
         permutation = permutation if permutation is not None else [1, 0]
         super().__init__([arg], {'permutation': permutation}, loc)
+
+
+class Relu(PlaiNode):
+    def __init__(self, arg: module.Node, loc: Location = None):
+        super().__init__([arg], {}, loc)
+
+
+class AddMm(PlaiNode):
+    def __init__(self, bias: module.Node, mat1: module.Node, mat2: module.Node, beta, alpha, loc: Location = None):
+        """
+        out = beta * bias + alpha * (mat1 * mat2)
+        """
+        super().__init__([bias, mat1, mat2], {'beta': beta, 'alpha': alpha}, loc)
+
+    @classmethod
+    def get_cls_name(cls):
+        return 'add_mm'
+
+
+def register_dialect():
+    pass  # do nothing, only for registration this file.
