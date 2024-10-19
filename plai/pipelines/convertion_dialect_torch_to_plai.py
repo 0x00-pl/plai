669893580f8f3f069dc0f0c71342ae5850cb1bf7
0x00-pl <0x00.pl@gmail.com>
2024-10-19 22:46:58 +0800

    fix bug add more converter on convertion_dialect_torch_to_plai.py

--- plai/pipelines/convertion_dialect_torch_to_plai.py
+++ plai/pipelines/convertion_dialect_torch_to_plai.py
@@ -1,5 +1,6 @@
 from plai import dialect
 from plai.core import module, pipeline, rewrite_pattern
+from plai.dialect import plai_dialect
 
 
 class ConvertTranspose(rewrite_pattern.TypedRewritePattern):
@@ -9,8 +10,38 @@ class ConvertTranspose(rewrite_pattern.TypedRewritePattern):
     def match_and_replace(self, graph: module.Graph, node: module.Node) -> bool:
         assert isinstance(node, dialect.aten_dialect.Transpose)
         graph.set_insert_point_after(node)
-        new_node = dialect.plai_dialect.Transpose(node.operands[0])
+        new_node = plai_dialect.Transpose(node.operands[0])
         graph.add_node(new_node)
+        graph.replace_all_uses_with(node, new_node)
+        graph.remove_node(node)
+        return True
+
+
+class ConvertRelu(rewrite_pattern.TypedRewritePattern):
+    def __init__(self):
+        super().__init__(dialect.aten_dialect.Relu)
+
+    def match_and_replace(self, graph: module.Graph, node: module.Node) -> bool:
+        assert isinstance(node, dialect.aten_dialect.Relu)
+        graph.set_insert_point_after(node)
+        new_node = plai_dialect.Relu(node.operands[0])
+        graph.add_node(new_node)
+        graph.replace_all_uses_with(node, new_node)
+        graph.remove_node(node)
+        return True
+
+
+class ConvertAddmm(rewrite_pattern.TypedRewritePattern):
+    def __init__(self):
+        super().__init__(dialect.aten_dialect.Addmm)
+
+    def match_and_replace(self, graph: module.Graph, node: module.Node) -> bool:
+        assert isinstance(node, dialect.aten_dialect.Addmm)
+        graph.set_insert_point_after(node)
+        new_node = plai_dialect.AddMm(node.operands[0], node.operands[1], node.operands[2], node.attrs['beta'],
+                                      node.attrs['alpha'])
+        graph.add_node(new_node)
+        graph.replace_all_uses_with(node, new_node)
         graph.remove_node(node)
         return True
 
@@ -19,11 +50,11 @@ class TorchToPlaiPass(pipeline.Pass):
     def __init__(self):
         super().__init__('torch_to_plai')
 
-    def __call__(self, graph) -> bool:
+    def __call__(self, graph: module.Graph) -> bool:
         """
         :param graph:
         :return: True when changed.
         """
-        pattern_list = rewrite_pattern.RewritePatternList([ConvertTranspose()])
+        pattern_list = rewrite_pattern.RewritePatternList([ConvertTranspose(), ConvertRelu(), ConvertAddmm()])
         changed = rewrite_pattern.rewrite_pattern_recursive(graph, pattern_list)
         return changed
