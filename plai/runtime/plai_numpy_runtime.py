17b9ba82b73fcedfed4791dab491f6704f0e5238
0x00-pl <0x00.pl@gmail.com>
2024-11-10 16:12:35 +0800

    add code on plai_numpy_runtime.py

--- plai/runtime/plai_numpy_runtime.py
+++ plai/runtime/plai_numpy_runtime.py
@@ -1,20 +1,22 @@
 import numpy
 
 from plai.core import runtime
-from plai.dialect.plai_dialect import Transpose, MatMul
+from plai.dialect.plai_dialect import Transpose, MatMul, Add, Relu
 
 
 class PlaiNumpyRuntime(runtime.Runtime):
     def run(self, graph, input_tensors):
-        print(graph)  # debug
-
         node_value_dict = dict(zip(graph.arguments, input_tensors))
         for node in graph.nodes:
             operand_values = [node_value_dict[operand] for operand in node.operands]
             if isinstance(node, Transpose):
                 result = numpy.transpose(operand_values[0], axes=node.attrs['permutation'])
-            if isinstance(node, MatMul):
+            elif isinstance(node, MatMul):
                 result = numpy.matmul(operand_values[0], operand_values[1])
+            elif isinstance(node, Add):
+                result = operand_values[0] + operand_values[1]
+            elif isinstance(node, Relu):
+                result = numpy.maximum(operand_values[0], 0)
             else:
                 raise NotImplementedError(f"Node {node} is not supported by NumpyRuntime")
 
