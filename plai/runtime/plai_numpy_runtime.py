7ee2af85a4f811822fc5f3d1243d2e3c2af1677b
0x00-pl <0x00.pl@gmail.com>
2024-11-29 15:34:43 +0800

    fix test warning

--- plai/runtime/plai_numpy_runtime.py
+++ plai/runtime/plai_numpy_runtime.py
@@ -1,18 +1,23 @@
 import typing
 
 import numpy
+import torch
 
 from plai.core import runtime
+from plai.core.core_dialect import Output
 from plai.core.node import Node
 from plai.dialect.plai_dialect import Transpose, MatMul, Add, Relu
 
 
 class PlaiNumpyRuntime(runtime.Runtime):
     def run(self, graph, input_tensors):
-        node_value_dict: typing.Dict[Node, typing.Any] = dict(zip(graph.arguments, input_tensors))
+        node_value_dict: typing.Dict[Node, numpy.ndarray] = {k: v.cpu().numpy() for k, v in
+                                                             zip(graph.arguments, input_tensors)}
         for node in graph.nodes:
             operand_values = [node_value_dict[operand] for operand in node.operands]
-            if isinstance(node, Transpose):
+            if isinstance(node, Output):
+                result = None
+            elif isinstance(node, Transpose):
                 result = numpy.transpose(operand_values[0], axes=node.attrs['permutation'])
             elif isinstance(node, MatMul):
                 result = numpy.matmul(operand_values[0], operand_values[1])
@@ -25,5 +30,4 @@ class PlaiNumpyRuntime(runtime.Runtime):
 
             node_value_dict[node] = result
 
-        print(graph)
-        return [node_value_dict[output] for output in graph.outputs.operands]
+        return [torch.from_numpy(node_value_dict[output]) for output in graph.outputs.operands]
