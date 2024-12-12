5b8537974ebca3a8aa225d4b8349d46a721c580f
0x00-pl <0x00.pl@gmail.com>
2024-12-12 15:07:25 +0800

    add tuple type

--- plai/core/type_notation.py
+++ plai/core/type_notation.py
@@ -2,7 +2,7 @@ import typing
 
 
 class TypeNotation:
-    def __init__(self, name: str):
+    def __init__(self, name: str = '?'):
         self.name = name
 
     def __str__(self):
@@ -21,9 +21,18 @@ class ScalarType(TypeNotation):
 
 class TensorType(TypeNotation):
     def __init__(self, shape: typing.Sequence[int], element_type: TypeNotation):
-        super().__init__('tensor')
+        super().__init__()
         self.shape = shape
         self.element_type = element_type
 
     def __str__(self):
         return f'tensor({self.shape}, {self.element_type})'
+
+
+class TupleType(TypeNotation):
+    def __init__(self, types: typing.Sequence[TypeNotation]):
+        super().__init__()
+        self.types = types
+
+    def __str__(self):
+        return f'tuple({", ".join(str(t) for t in self.types)})'
