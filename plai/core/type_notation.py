9f0d0b616cfc07b546dd7db5d5ed86bd55fa81b0
0x00-pl <0x00.pl@gmail.com>
2024-12-25 15:52:19 +0800

    add broadcast_shape

--- plai/core/type_notation.py
+++ plai/core/type_notation.py
@@ -40,3 +40,23 @@ class TupleType(TypeNotation):
 
     def __str__(self):
         return f'tuple({", ".join(str(t) for t in self.types)})'
+
+
+def broadcast_shape(shape1: typing.List[int], shape2: typing.List[int]) -> typing.List[int]:
+    """
+    Broadcast two shapes together.
+    """
+    result = []
+    len1, len2 = len(shape1), len(shape2)
+    for i in range(max(len1, len2)):
+        dim1 = shape1[len1 - 1 - i] if i < len1 else 1
+        dim2 = shape2[len2 - 1 - i] if i < len2 else 1
+        if dim1 == dim2:
+            result.append(dim1)
+        elif dim1 == 1:
+            result.append(dim2)
+        elif dim2 == 1:
+            result.append(dim1)
+        else:
+            raise ValueError(f'Incompatible shapes: {shape1}, {shape2}')
+    return list(reversed(result))
